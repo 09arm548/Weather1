194aca25ea4f238c0f14d4e13eb7ac874204e584
AndreaMcPherson <arm20@sage.southhills.edu>
2010-09-30 14:32:53 -0400

    xml hints

--- Form1.vb
+++ Form1.vb
@@ -2,6 +2,6 @@
 
    
     Private Sub btnGetWeather_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnGetWeather.Click
-
+        'figure out 3 calls to get weather ' 3 method calls from the xml reader call
     End Sub
 End Class

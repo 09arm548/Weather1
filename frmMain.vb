6136b95c8ce50e204653e48f8e5df833856aed70
09arm548 <arm20@sage.southhills.edu>
2010-10-05 19:31:27 -0400

    I got the weather!!!!!!!!!

--- frmMain.vb
+++ frmMain.vb
@@ -4,30 +4,24 @@ Imports System.Xml
 Public Class frmMain
 
 
+
     Private Sub btnGetWeather_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnGetWeather.Click
-        '''I NEED TO:
-        ''' CREATE A XML READER
-        ''' FIGURE OUT HOW TO READ XML
-        ''' AND LOOP THRU TO FIND THE WEATHER
 
-        'Dim stream As System.IO.StreamReader = New StreamReader("www.weather.com")
-
-        'MessageBox.Show(stream.ReadLine)
-        ' Loads the XML data in the file books.xml in a new stream.
-        'stream = New StreamReader("http://weather.yahooapis.com/forecastrss?w=2502265")
-
-        'ListBox1.Items.Add(stream)
+        Dim stream As XmlReader = XmlReader.Create("http://weather.yahooapis.com/forecastrss?p=USPA0031&amp;u=f")
+        Dim anything As Boolean
+        Dim temp As String
 
 
-        Dim rssURL As String = "http://weather.yahooapis.com/forecastrss?w=2502265"
-        Dim myRequest As System.Net.WebRequest = System.Net.WebRequest.Create(rssURL)
-        'ListBox1.Items.Add(rssURL)
-        'Dim myResponse As System.Net.WebResponse = myRequest.GetResponse
-        'Dim rssStream As System.IO.Stream = myResponse.GetResponseStream()
-        'Dim rssDoc As System.Xml.XmlDocument = New System.Xml.XmlDocument()
-        'rssDoc.Load(rssStream)
-        ' Dim rssItems As System.Xml.XmlNodeList = rssDoc.SelectNodes
-        'figure out 3 calls to get weather ' 3 method calls from the xml reader call
-        'github to work 
+        Do Until anything = True
+            anything = stream.ReadToFollowing("yweather:condition")
+        Loop
+        temp = stream.GetAttribute("temp")
+
+
+
+        MessageBox.Show(temp)
+
+
+
     End Sub
 End Class
